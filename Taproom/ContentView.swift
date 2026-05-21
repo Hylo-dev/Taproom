8cc97707be45ecaea57fb90473b8f9e008903005
C4V4.exe <104673028+C4V4H@users.noreply.github.com>
2026-05-21 12:55:33 +0200

    Initial Commit

---
+++ Taproom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taproom
+//
+//  Created by C4V4H.exe on 21/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taproom/TaproomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaproomApp.swift
+//  Taproom
+//
+//  Created by C4V4H.exe on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaproomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
